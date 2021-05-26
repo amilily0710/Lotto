b9ecf622520cd7852ec9e45a55407948bf4d238c
amili <amilily0710@naver.com>
2021-05-26 13:27:08 +0900

    Random number generation

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -4,23 +4,58 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.cardview.widget.CardView
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+fun getRandomLottoNumber (): Int {
+    return Random().nextInt(45) + 1
+}
+fun getRandomLottoNumbers() : MutableList<Int> {
+    val lottoNumbers = mutableListOf<Int>()
+
+    while(true){
+        var number: Int = getRandomLottoNumber()
+        var flag_existing: Int = 0
+       // for(j in 0..lottoNumbers.size){
+            if(lottoNumbers.contains(number)){
+             //   flag_existing = 1
+                continue;
+
+        }
+     //   if (flag_existing.equals(1))
+      //      continue
+       // else
+            lottoNumbers.add(number)
+        if(lottoNumbers.size >= 6)
+            break;
+    }
+    return lottoNumbers}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         findViewById<View>(R.id.CardNumber).setOnClickListener {
             val intent = Intent(this, ResultActivity::class.java)
-            startActivity(intent)}
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+            startActivity(intent)
+        }
 
-      findViewById<View>(R.id.ConstellationCard).setOnClickListener {
-          val intent = Intent(this, ConstellationActivity::class.java)
-          startActivity(intent)}
+        findViewById<View>(R.id.ConstellationCard).setOnClickListener {
+            val intent = Intent(this, ConstellationActivity::class.java)
+            startActivity(intent)
+        }
 
         findViewById<View>(R.id.NumberGeneration).setOnClickListener {
             val intent = Intent(this, NameActivity::class.java)
             startActivity(intent)
-      }
+        }
+
     }
+
 }
+
